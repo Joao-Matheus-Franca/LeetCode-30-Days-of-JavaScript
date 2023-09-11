e121f61e404ef8d4c18330883fbfd549b3e26cba
João Matheus Silva Franca <matheussfranca@live.com>
2023-09-11 16:14:02 -0300

    Adicionando questão To Be Or Not To Be

--- closures.js
+++ closures.js
@@ -1,11 +1,11 @@
-// Create Hello World Function:
+// Create Hello World Function(Easy):
 	let createHelloWorld = function() {
 		return function(...args) {
 				return "Hello World"
 		}
 	};
 
-// Counter:
+// Counter(Easy):
 	let createCounter = function(n) {
 		let count = 0;
 		return function() {
@@ -16,3 +16,21 @@
 			return n + (count - 1);
 		};
 	};
+
+//To Be Or Not To Be(Easy):
+var expect = function(val) {
+	return {
+		toBe: function(value) {
+			if (val === value) {
+				return true
+			}
+				throw new Error('Not Equal')
+		},
+		notToBe: function(value) {
+			if (val !== value) {
+				return true
+			}
+			throw new Error('Equal')
+		}
+	}	
+};
