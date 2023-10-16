ee5fbd014d3c3748a5004bdf02f8a576bf014940
João Matheus Silva Franca <matheussfranca@live.com>
2023-10-16 09:43:53 -0300

    Adicionando exercício 02 sobre tranformação de arrays

--- basicArrayTransformations.js
+++ basicArrayTransformations.js
@@ -6,3 +6,15 @@ let map = function(arr, fn) {
     };
     return newArray;
 };
+
+//Filter Elements from Array(Easy):
+let filter = function(arr, fn) {
+    let newArray = [];
+    for(let i = 0; i < arr.length; i += 1) {
+        if(fn(arr[i], i)) {
+            newArray.push(arr[i])
+            };
+    };
+    return newArray;
+ };
+
